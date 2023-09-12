8339da604b1fe5e899aa9743105ad839815b1f39
Yannick Gouez <ygouez5757@gmail.com>
2023-09-12 15:10:51 +0200

    ajout d'authentification

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+    return (
+        <div className=" flex justify-center align-center">
+            <SignUp/>
+        </div>
+    );
+}
+ 
+export default SignUpPage;
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>Leitner env</div>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
