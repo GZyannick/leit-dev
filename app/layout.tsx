3d52e05b1f139db4b9b94ab56f5c42f82d9a561e
Yannick Gouez <ygouez5757@gmail.com>
2023-09-13 13:11:18 +0200

    ajout de l'a base de l'ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="system"  enableSystem>
             {children}
       </ThemeProvider>
       </body>
