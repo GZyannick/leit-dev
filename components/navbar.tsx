0580b19d27599ca87c7ed9fc36fc6652e54b4c2a
Yannick Gouez <ygouez5757@gmail.com>
2023-11-08 13:49:12 +0100

    navbar style change

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+// TODO CHANGE COLOR TO GENERAL DARK COLOR
 import { Menu } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
@@ -13,13 +13,13 @@ const Navbar = () => {
 
   const menus = [
     { title: "Mindmap", path: "/mindmap" },
-    { title: "Leitner box", path: "/leitner-box" },
     { title: "Daily question", path: "/daily-question" },
+    { title: "Leitner", path: "/leitner-box" },
   ];
 
   return (
     <nav className="w-full px-6">
-      <div className="md:flex ">
+      <div className="items-center justify-between md:flex">
         <div className="flex items-center justify-between py-3 md:block md:py-5">
           <Link href="/" className="text-3xl">
             <p className={monoton.className}>LD</p>
@@ -34,13 +34,18 @@ const Navbar = () => {
           </div>
         </div>
         <div
-          className={`mt-8 flex-1 justify-self-center pb-3 md:mt-0 md:block md:pb-0 ${
+          className={`mt-8 justify-self-center pb-3 md:mt-0 md:block md:pb-0 ${
             state ? "block" : "hidden"
           }`}
         >
-          <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+          <ul className="items-center justify-center space-y-8 rounded-full px-8 py-3 md:flex md:space-x-6 md:space-y-0 md:border-2 md:border-[#2E2E32]	md:bg-[#242426] md:shadow-[-2px_-2px_4px_rgba(223,223,223,0.15)]">
             {menus.map((item, idx) => (
-              <li key={idx} className="text-gray-600 hover:text-indigo-600">
+              <li
+                key={idx}
+                className={`text-[#D7D7D7] hover:text-indigo-600 ${
+                  idx === 1 ? "px-16" : ""
+                }`}
+              >
                 <Link href={item.path}>{item.title}</Link>
               </li>
             ))}
